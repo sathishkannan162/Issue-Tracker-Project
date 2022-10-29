3bec0826fe03bae98eb6bec4282bed081ebbc3a7
sathishkannan162 <sathishkannan162@gmail.com>
2022-10-29 22:36:24 +0530

    chore: sample data

---
+++ database/sampleIssues.js
@@ -0,0 +1,41 @@
+module.exports = [
+    { 
+      "issue_title": "Fix bug in authentication",
+      "issue_text": "Auth",
+      "created_by": "Kan",
+      "assigned_to": "Kan",
+      "open": true,
+      "status_text": "Not Started"
+    },
+    { 
+        "issue_title": "Bug in UI",
+        "issue_text": "Button not working",
+        "created_by": "Akash",
+        "assigned_to": "Joe",
+        "open": true,
+        "status_text": "In progress"
+      },
+      { 
+        "issue_title": "Database connection error",
+        "issue_text": "App Starting",
+        "created_by": "Bear",
+        "assigned_to": "Eli",
+        "open": true,
+        "status_text": "Not started"
+      },
+      { 
+        "issue_title": "Error in API",
+        "issue_text": "When we send GET Request with query",
+        "created_by": "Maple",
+        "assigned_to": "Akash",
+        "open": true,
+        "status_text": "In progress"
+      },{ 
+        "issue_title": "Fix error in function",
+        "issue_text": "Results not matching",
+        "created_by": "Bale",
+        "assigned_to": "Bale",
+        "open": false,
+        "status_text": "Closed"
+      },
+   ]
