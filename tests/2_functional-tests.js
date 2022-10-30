09b9eafee60dac48822ad2110221b6f56ea4ae9d
sathishkannan162 <sathishkannan162@gmail.com>
2022-10-30 21:06:36 +0530

    test: GET request to api

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -2,13 +2,24 @@ const chaiHttp = require("chai-http");
 const chai = require("chai");
 const assert = chai.assert;
 const server = require("../server");
+const myDB = require('../database/connection');
 
 chai.use(chaiHttp);
 
-suite("Functional Tests", function () {
+suite("Functional Tests", async function () {
   this.timeout(5000);
+  // delete test records
+  await myDB(IssueModel=>{
+    IssueModel.deleteMany({project: 'chaiTests'})
+    .then(docs=>{
+    console.log('deleted:', docs);
+    })
+    .catch(err=>{
+    console.log(err)
+    });
+  });
 
-  suite("Test all fields POST api/project/hello", function () {
+  suite("Test all fields POST api/project/chaiTests", function () {
     test("fill all fields in FORM 1", function (done) {
       const message = {
         issue_title: "chai post all fields",
@@ -19,7 +30,7 @@ suite("Functional Tests", function () {
       };
       chai
         .request(server)
-        .post("/api/issues/apitest")
+        .post("/api/issues/chaiTests")
         .send(message)
         .end(function (err, res) {
           assert.equal(res.status, 200);
@@ -31,13 +42,13 @@ suite("Functional Tests", function () {
           assert.equal(res.body.assigned_to, message.assigned_to);
           assert.equal(res.body.status_text, message.status_text);
           assert.isString(res.body._id);
-          assert.equal(res.body.project, "apitest");
+          assert.equal(res.body.project, "chaiTests");
           done();
         });
     });
   });
 
-  suite("Test required fields POST api/project/hello", function () {
+  suite("Test required fields POST api/project/chaiTests", function () {
     test("fill required fields in FORM 1", function (done) {
       const message = {
         issue_title: "chai post required fields",
@@ -46,7 +57,7 @@ suite("Functional Tests", function () {
       };
       chai
         .request(server)
-        .post("/api/issues/apitest")
+        .post("/api/issues/chaiTests")
         .send(message)
         .end(function (err, res) {
           assert.equal(res.status, 200);
@@ -56,13 +67,13 @@ suite("Functional Tests", function () {
           assert.equal(res.body.issue_text, message.issue_text);
           assert.equal(res.body.created_by, message.created_by);
           assert.isString(res.body._id);
-          assert.equal(res.body.project, "apitest");
+          assert.equal(res.body.project, "chaiTests");
           done();
         });
     });
   });
 
-  suite("Test missing required fields POST api/project/hello", function () {
+  suite("Test missing required fields POST api/project/chaiTests", function () {
     test("fill with missing required fields in FORM 1", function (done) {
       const message = {
         issue_title: "chai post with missing required fields",
@@ -72,12 +83,34 @@ suite("Functional Tests", function () {
         we can check whether there is error in every field */
       chai
         .request(server)
-        .post("/api/issues/apitest")
+        .post("/api/issues/chaiTests")
         .send(message)
         .end(function (err, res) {
+          assert.equal(res.status, 200);
           assert.equal(res.body.name, "ValidationError");
           done();
         });
     });
   });
+  suite('TeSt GET request to /api/issues/{project}',function(){
+    test('TeSt GET request to /api/issues/{project}', function(done){
+      chai
+      .request(server)
+      .get('/api/issues/chaiTests')
+      .end(function(err,res){
+        console.log(res.body);
+        assert.equal(res.body.length,2);
+        assert.equal(res.body[0].issue_title,'chai post all fields');
+        assert.equal(res.body[1].issue_title,'chai post required fields');
+        assert.equal(res.body[0].created_by,'chai');
+        assert.equal(res.body[1].created_by,'chai');
+
+        done();
+      })
+    })
+  });
+     
+  
 });
+
+
