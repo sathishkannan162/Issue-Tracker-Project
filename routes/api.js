37d7b55d45a49c6871c4f5c489b1d7ce632f94d3
sathishkannan162 <sathishkannan162@gmail.com>
2022-10-31 11:25:17 +0530

    refactor: req.body._id is required in put requests and removed console.log() messages

--- routes/api.js
+++ routes/api.js
@@ -7,17 +7,13 @@ module.exports = function (app,IssueModel) {
     .get(function (req, res){
       let project = req.params.project;
       let query = req.query
-      console.log(req.query);
       let issueFields = ["_id","issue_title","issue_text","created_on","updated_on","created_by","assigned_to","open","status_text"];
       for (var key in req.query) {
         if (!issueFields.includes(key)) {
           delete query[key];
         }
       }
-      query.project = project; 
-      console.log(query)
-      
-      
+      query.project = project;       
       IssueModel.find(query,{ project: 0, __v: 0})
       .then(docs=>{
         console.log(`sent project: ${project} issues to api`);
@@ -43,22 +39,25 @@ module.exports = function (app,IssueModel) {
     
     .put(function (req, res){
       let project = req.params.project;
+      if (req.body._id) {
       console.log(req.body);
+      req.body.updated_on = new Date();
       IssueModel.findOneAndUpdate({
         _id: req.body._id,
       },{
-        $set:{
-          open: req.body.open,
-          updated_on: new Date()
-        },
+        $set:req.body,
       },{new: true})
       .then(docs=>{
-      console.log('found', docs);
+      console.log('updated', docs);
         res.json(docs);
       })
       .catch(err=>{
       console.log(err);
       });
+    }
+    else {
+      res.send('_id required');
+    }
     })
     
     .delete(function (req, res){
@@ -67,7 +66,7 @@ module.exports = function (app,IssueModel) {
         _id: req.body._id
       })
       .then(docs=>{
-      console.log('delted',docs);
+      console.log('deleted',docs);
       res.json(docs);
       })
       .catch(err=>{
